591eb8628bfa835d70c890ea2999a263b9c5679d
Avneet Sidhu <avneet.sidhu@ryerson.ca>
2022-10-02 15:48:43 -0400

    handled errors

--- App.py
+++ App.py
@@ -1,3 +1,4 @@
+from importlib.abc import ResourceLoader
 import secrets
 from flask import Flask, request
 from movieClient import MovieClient
@@ -27,36 +28,53 @@ def health():
 @auth.login_required
 def getPopularMovies():
     result = movieClient.getReq(BASE_URL + '3/movie/popular?api_key=' + API_KEY + '&language=en-US&page=1')
-    content = result.content.decode("utf-8")
-    jsonFormat = json.loads(content)
-    return json.dumps(jsonFormat['results']), 200
+    if result.status_code == 200:
+        content = result.content.decode("utf-8")
+        jsonFormat = json.loads(content)
+        return json.dumps(jsonFormat['results']), 200
+    return 'something went wrong', 500
 
 @app.route("/get-similar-movies")
 @auth.login_required
 def getSimilarMovies():
-    id = str(request.args.get('id'))
+    if request.args.get('id'):
+        id = str(request.args.get('id'))
+    else:
+        return 'missing required arguments', 422
     result = movieClient.getReq(BASE_URL + '3/movie/' + id + '/similar?api_key=' + API_KEY + '&language=en-US&page=1')
-    content = result.content.decode("utf-8")
-    jsonFormat = json.loads(content)
-    return json.dumps(jsonFormat['results'][0:5]), 200
+    if result.status_code == 200:
+        content = result.content.decode("utf-8")
+        jsonFormat = json.loads(content)
+        return json.dumps(jsonFormat['results'][0:5]), 200
+    return 'something went wrong', 500
 
 @app.route("/get-movie")
 @auth.login_required
 def getMovieByID():
-    id = str(request.args.get('id'))
+    if request.args.get('id'):
+        id = str(request.args.get('id'))
+    else:
+        return 'missing required arguments', 422
     result = movieClient.getReq(BASE_URL + '3/movie/' + id + '?api_key=' + API_KEY + '&language=en-US&page=1')
-    content = result.content.decode("utf-8")
-    return content, 200
+    if result.status_code == 200:
+        content = result.content.decode("utf-8")
+        return content, 200
+    else:
+        return 'something went wrong', 500
 
 @app.route("/get-search-results")
 @auth.login_required
 def getSearchResults():
-    query = request.args.get('query')
+    if request.args.get('query'):
+        query = request.args.get('query')
+    else:
+        return 'missing required arguments', 422
     result = movieClient.getReq(BASE_URL + '3/search/movie?api_key=' + API_KEY + '&language=en-US&query=' + query + '&page=1&include_adult=false')
-    content = result.content.decode("utf-8")
-    jsonFormat = json.loads(content)
-    return json.dumps(jsonFormat['results'][0:20]), 200
-    
+    if result.status_code == 200:
+        content = result.content.decode("utf-8")
+        jsonFormat = json.loads(content)
+        return json.dumps(jsonFormat['results'][0:20]), 200
+    return 'something went wrong', 500
 if __name__ == '__main__':
     app.run()
 
